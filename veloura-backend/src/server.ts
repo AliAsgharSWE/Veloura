b30530ee9b30e96e8b35adbb4cfd76be5fb8396f
AliAsgharGill <aliasghar@algotx.ai>
2025-07-24 08:26:05 -0400

    feat: add MongoDB connection setup with mongoose and update dependencies

--- veloura-backend/src/server.ts
+++ veloura-backend/src/server.ts
@@ -1,6 +1,13 @@
 import app from "./app";
+import connectDB from "./config/db";
+import dotenv from "dotenv";
+dotenv.config();
+
+
 
 const PORT = process.env.PORT || 5000;
+// Connect to MongoDB
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
